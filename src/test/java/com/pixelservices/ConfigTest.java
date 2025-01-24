582dd727ff5a29e1bec21c3191c30fc3b498e8b1
SIEA <69807609+DrVosss@users.noreply.github.com>
2025-01-25 00:21:48 +0100

    "Add cleanup step to ConfigTest to delete generated config.yml file after test execution

--- src/test/java/com/pixelservices/ConfigTest.java
+++ src/test/java/com/pixelservices/ConfigTest.java
@@ -4,13 +4,21 @@ import com.pixelservices.config.YamlConfig;
 import com.pixelservices.logger.Logger;
 import com.pixelservices.logger.LoggerFactory;
 import org.junit.Test;
+import java.io.File;
 
 public class ConfigTest {
     private static final Logger logger = LoggerFactory.getLogger(ConfigTest.class);
 
     @Test
     public void testConfig() {
-        YamlConfig config = new YamlConfig("config.yml");
+        String filePath = "config.yml";
+        YamlConfig config = new YamlConfig(filePath);
         logger.info(config.getString("test"));
+
+        // Clean up the file after the test
+        File file = new File(filePath);
+        if (file.exists()) {
+            file.delete();
+        }
     }
 }
